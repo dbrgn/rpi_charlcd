4c4ce37d8c390a49fdc1c9ec0941cef69e4bd42e
Danilo Bargen <gezuru@gmail.com>
2013-05-06 22:31:55 +0200

    Another constructor ifx

--- rpi_charlcd.py
+++ rpi_charlcd.py
@@ -67,11 +67,12 @@ class CharLCD(object):
         # an explicit GPIO interface to use
         if not GPIO:
             import RPi.GPIO as GPIO
-            self.GPIO = GPIO
-            self.pin_rs = pin_rs
-            self.pin_e = pin_e
-            self.pins_db = pins_db
-            self.GPIO.setmode(GPIO.BCM)
+            GPIO.setmode(GPIO.BCM)
+
+        self.GPIO = GPIO
+        self.pin_rs = pin_rs
+        self.pin_e = pin_e
+        self.pins_db = pins_db
 
         self.GPIO.setup(self.pin_e, GPIO.OUT)
         self.GPIO.setup(self.pin_rs, GPIO.OUT)
